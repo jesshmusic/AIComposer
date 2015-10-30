b7f88e3e814930561248532c6e30fd5b74de5579
Jess Hendricks <jess@fupduckonline.com>
2015-10-30 14:06:57 -0400

    Initial Commit

---
+++ AIComposer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AIComposer
+//
+//  Created by Jess Hendricks on 10/30/15.
+//  Copyright © 2015 Jess Hendricks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
